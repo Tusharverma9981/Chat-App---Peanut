e7cb8d37a5c0881bff280048f258c190c5c06adf
Tushar <tv4648551@gmail.com>
2024-12-03 22:58:49 +0530

    made the UI for the sign up and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route, Navigate } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
@@ -10,6 +10,7 @@ import { useAuthStore } from './store/useAuthStore';
 import { useEffect } from 'react';
 
 import { Loader } from 'lucide-react';
+import {Toaster} from "react-hot-toast";
 
 
 
@@ -32,12 +33,13 @@ function App() {
     <div>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/signup' element={<SignUpPage />}/>
-        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/' element={ authUser ? <HomePage /> : <Navigate to="/login"/>}/>
+        <Route path='/signup' element={!authUser ? <SignUpPage />: <Navigate to="/"/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage />: <Navigate to="/"/>}/>
         <Route path='/settings' element={<SettingsPage />}/>
-        <Route path='/profile' element={<ProfilePage />}/>
+        <Route path='/profile' element={ authUser ? <ProfilePage />: <Navigate to="/login"/>}/>
       </Routes>
+      <Toaster/>
     </div>
   )
 }
