d793881719d5b3476f86f0e805ad95a79319011f
Tushar <tv4648551@gmail.com>
2024-12-10 13:03:43 +0530

    added the profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,6 +30,15 @@ app.listen(PORT,()=>{
     console.log("Server is running on port:"+PORT);
     connectDB()
 });
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
 
 
 
